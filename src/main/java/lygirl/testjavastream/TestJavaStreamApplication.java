caba1946c93b6e676d1f1167f8725da8841b95c2
YunGeol <lygirl@naver.com>
2018-11-18 23:06:03 +0900

    initial project settings maked by start.spring.io

---
+++ src/main/java/lygirl/testjavastream/TestJavaStreamApplication.java
@@ -0,0 +1,11 @@
+package lygirl.testjavastream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJavaStreamApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestJavaStreamApplication.class, args);
+    }
+}
